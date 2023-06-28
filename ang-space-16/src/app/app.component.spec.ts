bfe764024b083a49abd9bb91e9e9685d51635d22
Ivan Gamanovich <igamanovich@elinext.com>
2023-06-28 11:45:25 +0300

    init: jest with reporter

--- ang-space-16/src/app/app.component.spec.ts
+++ ang-space-16/src/app/app.component.spec.ts
@@ -8,18 +8,6 @@ describe('AppComponent', () => {
     declarations: [AppComponent]
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ang-space-16'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-space-16');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
