4b0b8204e98a6eabca49a6af49231da4caeea339
Ivan Gamanovich <igamanovich@elinext.com>
2023-06-30 11:56:58 +0300

    feat: add new part

--- ang-space-16/src/app/app.module.ts
+++ ang-space-16/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BooksService} from "./books/books.service";
 import {StoreModule} from '@ngrx/store';
 import {EffectsModule} from "@ngrx/effects";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
